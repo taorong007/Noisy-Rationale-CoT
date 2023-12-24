b29c67b067d0423dd6e7ed1fa09578d7a1a5dd37
taorong007 <2274298316@qq.com>
2023-12-24 19:19:34 +0800

    update experiment

--- data_process/tracking_shuffled_objects/tracking_shuffled_objects.py
+++ data_process/tracking_shuffled_objects/tracking_shuffled_objects.py
@@ -10,7 +10,7 @@ from collections import deque
 import math
 
 class tracking_shuffled_objects():
-    def __init__(self, n_shots=0, n_noisy_shots=0, noise_type="irrelevant", noise_ratio = 0.5, noise_distribution = "fixed", prefix_context =True, config: dict = None, reasoning_type = "length") -> None:
+    def __init__(self, n_shots=0, n_noisy_shots=0, noise_type="irrelevant", noise_ratio = 0.5, noise_distribution = "fixed", prefix_context =True, config: dict = None, obj_num = 3) -> None:
         
         self.n_shots = n_shots
         self.n_noisy_shots = n_noisy_shots
@@ -31,13 +31,14 @@ class tracking_shuffled_objects():
             self.noise_ratio = 0
             self.noise_distribution = None
         if config is not None:
-            self.reasoning_type = config["reasoning_type"]
+            self.obj_num = config["obj_num"]
         else:
-            self.reasoning_type = reasoning_type
+            self.obj_num = obj_num
+        assert self.obj_num == 3 or self.obj_num == 5 or self.obj_num == 7
         
         self.file_path = os.path.join("data", "tracking_shuffled_objects")
         self.unzip_data()
-        self.init_noise_data()
+        # self.init_noise_data()
         return
 
     
@@ -51,7 +52,7 @@ class tracking_shuffled_objects():
             self.noise_facts[phrase] = facts
 
     def unzip_data(self):
-        zip_files = "SCAN-master.zip"
+        zip_files = "tracking_shuffled_objects.zip"
         unzip_path = os.path.join(self.file_path, "unzip_data")
         if not os.path.exists(unzip_path):
             os.makedirs(unzip_path)
@@ -75,26 +76,86 @@ class tracking_shuffled_objects():
                     print(f"No match found, {line}")
         return dataset
     
+    def pre_process(self, raw_data):
+        people_name = ["Alice", "Bob", "Claire", "Dave", "Eve"]
+        tasks = ["swap balls", "swap gifts", "swap positions", "trade books", "switch partners"]
+        have_words = [r"(\w+) has a (\w+ \w+)", r"(\w+) has a (\w+ \w+)", r"(\w+) is playing (\w+(?: \w+)?)",  r"(\w+) gets (.*?)[,.]",  r"(\w+) is dancing with (\w+)"]
+        swich_words = [r"(\w+) and (\w+) swap balls", r"(\w+) and (\w+) swap their gifts", r"(\w+) and (\w+) trade positions",  r"(\w+) and (\w+) swap books", r"(\w+) and (\w+) switch partners"]
+        target_person_words = [r"At the end of the game, (\w+) has the ", r"At the end of the event, (\w+) has the", r"At the end of the match, (\w+) is playing",
+                               r"At the end of the semester, (\w+) has ", r"At the end of the dance, (\w+) is dancing with"] 
+        
+        task_description = ["what kind of ball <target_person> has", "what kind of present <target_person> has", "what position <target_person> is playing", 
+                           "at the end of the semester, what book does <target_person> have", "at the end of the dance, who <target_person> is dancing with"]
+        answer_format = ["<color> ball.", "\"<color> present.\" or \"<color> ball.\"", "<position>.", "<name of book>.", "<name of person>."]
+        input = raw_data["input"]
+        for index, task in enumerate(tasks):
+            if task in input:
+                task_type = index
+                break
+        
+        pattern = re.compile(have_words[task_type])
+        starting_inventory = pattern.findall(input)
+        
+        pattern = re.compile(swich_words[task_type])
+        trade_log = pattern.findall(input)
+        
+        pattern = re.compile(target_person_words[task_type])
+        target_person = pattern.findall(input)[0]
+        
+        task_description = task_description[task_type].replace("<target_person>", target_person)
+        
+        raw_data["answers_domain"] = []
+        target_score = raw_data["target_scores"]
+        label = None
+        for key, value in target_score.items():
+            if value == 1:
+                label = key[:-1]
+            raw_data["answers_domain"].append(key[:-1])
+        if label == None:
+            raise ValueError("target score err")
+        
+        assert len(task_description) > 0 
+        raw_data["names"] = people_name[:self.obj_num]
+        raw_data["starting_inventory"] = starting_inventory
+        raw_data["trade_log"] = trade_log
+        raw_data["label"] = label
+        raw_data["task_description"] = task_description
+        raw_data["answer_format"] = answer_format[task_type]
+        raw_data["task_type"] = task_type
+        return
+    
+    def get_num(self, dataset):
+        people_name = ["Alice", "Bob", "Claire", "Dave", "Eve"]
+        tasks = ["swap balls", "swap gifts", "swap positions", "swap books", "switch partners"]
+        tasks_num = [0] * 5
+        for raw_data in dataset:
+            input = raw_data["input"]
+            for index, task in enumerate(tasks):
+                if task in input:
+                    tasks_num[index]+=1
+                    break
+        return tasks_num
+        
     def load_data(self):
-        # unzip_path = os.path.join(self.file_path, "unzip_data", str(self.trainset))
-        # trainset_file_name = f"{self.trainset}.2,{self.trainset}.3_train.csv"
-        # testset_file_name = f"{self.testset}_test.csv"
-        # self.trainset = pd.read_csv(os.path.join(unzip_path, trainset_file_name))
-        unzip_path = os.path.join(self.file_path, "unzip_data", f"{self.reasoning_type}_split")
-        if self.reasoning_type ==  "length":
-            train_file_name = "tasks_train_length.txt"
-            test_file_name = "tasks_test_length.txt"
-        if self.reasoning_type ==  "simple":
-            train_file_name = "tasks_train_simple.txt"
-            test_file_name = "tasks_test_simple.txt"
+        if self.obj_num == 3:
+            obj_num = "three"
+        elif self.obj_num == 5:
+            obj_num = "five"
         else:
-            raise ValueError(f"reasoning type{self.reasoning_type} not support")
+            obj_num = "seven"
+        unzip_path = os.path.join(self.file_path, "unzip_data", f"{obj_num}_objects")
         
-        with open(os.path.join(self.file_path, "base_example.json"), "r") as f:
-            self.base_example = json.load(f)
+        with open(os.path.join(unzip_path, "task.json"), "r") as f:
+            dataset = json.load(f)["examples"]
+    
+        for data in dataset:
+            self.pre_process(data)
         
-        trainset = self.read_raw_file(os.path.join(unzip_path, train_file_name))
-        testset = self.read_raw_file(os.path.join(unzip_path, test_file_name))
+        random.shuffle(dataset)
+        test_num = int(len(dataset)*0.8)
+        trainset = dataset[test_num:]
+        testset = dataset[:test_num]
+        # self.get_num(testset)
         self.trainset = trainset
         return testset
     
@@ -128,13 +189,15 @@ class tracking_shuffled_objects():
         return if_noise
         
     def get_question(self, raw_data):
-        in_content = raw_data[0]
-        question = f"With IN:{in_content}, what is OUT?\n"
-        question += "Please reason it step by step, and provide the final action sequence as the answer. End the question with \"So, final answer is OUT: <action sequence>\"\n"
+        content = raw_data["input"]
+        answer_domain = raw_data["answers_domain"]
+        task_description = raw_data["task_description"]
+        question = f"{content}?\n"
+        question += f"Please reason it step by step, and answer {task_description}. The answer field is in the {str(answer_domain)} You must answer the question in the format of \"answer: <one of answer in the answer domain without other description>\"\n"
         return question
     
     def get_label(self, raw_data):
-        return str(raw_data[1])
+        return str(raw_data["label"])
         
     def get_random_fact(self, rephrase, selected_set):
         facts = self.noise_facts[rephrase]
@@ -148,48 +211,54 @@ class tracking_shuffled_objects():
         return fact
     
         
-    def find_sentence_containing_strings(self, text, name1, name2):
-        sentences = text.split('.')
-        for sentence in sentences:
-            if name1 in sentence and name2 in sentence:
-                return sentence
-        return None
+    # def find_sentence_containing_strings(self, text, name1, name2):
+    #     sentences = text.split('.')
+    #     for sentence in sentences:
+    #         if name1 in sentence and name2 in sentence:
+    #             return sentence
+    #     return None
     
-    def error_action(self, origin_action):
-        actions = ["jump", "run", "walk", "look"]
-        while 1:
-            random_index = random.randrange(0, len(actions))
-            choose_action = actions[random_index]
-            if choose_action != origin_action:
-                break
-        return choose_action
+    # def error_action(self, origin_action):
+    #     actions = ["jump", "run", "walk", "look"]
+    #     while 1:
+    #         random_index = random.randrange(0, len(actions))
+    #         choose_action = actions[random_index]
+    #         if choose_action != origin_action:
+    #             break
+    #     return choose_action
     
-    def error_direction(self, direction):
-        if direction == "left":
-            return "right"
-        else:
-            return "left"
+    # def error_direction(self, direction):
+    #     if direction == "left":
+    #         return "right"
+    #     else:
+    #         return "left"
     
     
-    def _get_answer(self, in_content, ir_noise_distrib_state=None, mn_noise_distrib_state=None):    
-        return answer, action_sequence, n_ir_pos, n_mn_pos
+    # def _get_answer(self, in_content, ir_noise_distrib_state=None, mn_noise_distrib_state=None):  
+    #     return answer, action_sequence, n_ir_pos, n_mn_pos
     
     def get_answer(self, raw_data, if_noise):
-        in_content = raw_data[0]
-        label = raw_data[1]
+        content = raw_data["input"]
+        label = raw_data["label"]
+        starting_inventory = raw_data["starting_inventory"]
+        trade_log = raw_data["trade_log"]
+        names = raw_data["names"]
+        if len(starting_inventory) == 0 or len(trade_log) ==0 :
+            raise ValueError("parse error")
+        target_person = raw_data["target_person"]
         
-        _, _, n_ir_pos, n_mn_pos = self._get_answer(in_content)
+        # _, _, n_ir_pos, n_mn_pos = self._get_answer(in_content)
         
         
-        if self.noise_type == "irrelevant":
-            ir_noise_p = self.noise_ratio
-            mn_noise_p = 0
-        else:
-            ir_noise_p = 0
-            mn_noise_p = self.noise_ratio
-        ir_noise_distrib_state = self._prepare_noise_distribution_iteration_state(n_ir_pos, ir_noise_p, self.noise_distribution) 
-        mn_noise_distrib_state = self._prepare_noise_distribution_iteration_state(n_mn_pos, mn_noise_p, self.noise_distribution)                 
-        answer,action_sequence, _, _ = self._get_answer(in_content, ir_noise_distrib_state, mn_noise_distrib_state)
+        # if self.noise_type == "irrelevant":
+        #     ir_noise_p = self.noise_ratio
+        #     mn_noise_p = 0
+        # else:
+        #     ir_noise_p = 0
+        #     mn_noise_p = self.noise_ratio
+        # ir_noise_distrib_state = self._prepare_noise_distribution_iteration_state(n_ir_pos, ir_noise_p, self.noise_distribution) 
+        # mn_noise_distrib_state = self._prepare_noise_distribution_iteration_state(n_mn_pos, mn_noise_p, self.noise_distribution)                 
+        # answer,action_sequence, _, _ = self._get_answer(in_content, ir_noise_distrib_state, mn_noise_distrib_state)
         
         # print(action_sequence)
         assert str(action_sequence) == str(label)
@@ -205,7 +274,7 @@ class tracking_shuffled_objects():
         qustion  = self.get_question(raw_data)
         label = self.get_label(raw_data)
         shots = []
-        system_prompt = self.get_sys_prompt()
+        # system_prompt = self.get_sys_prompt()
         prefix = ""
         if(self.if_in_context):
             n_shots = self.n_shots
@@ -234,17 +303,15 @@ class tracking_shuffled_objects():
                     prefix += "user:"
                 else:    
                     case["in-context"] = shots
-                    case["system-prompt"] = system_prompt
                     
         case["question"] = prefix + qustion
         case["label"] = label
         return case
     
     def match_answer(self, answer_str):
-        match = re.search(r'OUT:\s*(.*)', answer_str)
+        match = re.search(r'[Aa]nswer:\s*(.*)', answer_str)
         if match:
-            squence_str = re.sub(r'[^a-zA-Z0-9_\s]+', '', match.group(1)).strip()
-            squence = squence_str.split()
+            squence = match.group(1)
             print("match: " + str(squence))
             return str(squence)
         else:
